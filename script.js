e05998faf6843a70363167d79b85ecbb13fa3b10
jerrsky8997 <jerremyfm71@gmail.com>
2026-04-23 14:00:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var Nim = "15240505";
+var Nama = "Jeremy Febrian Manuputty"
+var Usia = "20 Tahun"
+var Hobi = "Galau"
+
+document.getElementById("Nim").innerHTML = "Nim : " + Nim;
+document.getElementById("Nama").innerHTML = "Nama : " + Nama;
+document.getElementById("Usia").innerHTML = "Usia : " + Usia;
+document.getElementById("Hobi").innerHTML = "Hobi : " + Hobi;
+document.getElementById("Portofolio").textContent = "Halo, perkenalkan nama saya " + Nama + " Umur " + Usia;
